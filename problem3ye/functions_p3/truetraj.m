e4418e06842611cb3aa371758b8f2d23a71facdf
yejun1204 <yejun1204@gmail.com>
2019-12-16 23:12:11 +0900

    problem 3 trajectory/error plot

---
+++ problem3ye/functions_p3/truetraj.m
@@ -0,0 +1,75 @@
+%% function to provide the true traj and measurements
+
+function [x,t,z] = truetraj(seed)
+    if nargin<1
+        seed = 1;
+    end
+    rng(seed);
+    
+    PHR2PSEC = 1/3600; % km/hr (per hour) to km/s (per sec)
+    %% constants
+    % Ts = 1
+    dt = 1;
+
+    % radius of the trajectory
+    R0 = 10;
+    
+    % origin of the ''
+    Origin = [20;30];
+    
+    % initial velocity
+    V0 = PHR2PSEC * 50;    
+    
+    % initial yaw
+    THETA0 = 0;
+    
+    % initial position
+    x0 = Origin + R0 * [cos(THETA0); sin(THETA0)];
+    
+    % measurement noise std
+    nu = 1 * DEG2RAD;
+    
+    %% variables
+    % with some margin
+    t = 0:dt:120*60;
+        
+    % allocation
+    x = zeros(2,length(t));
+    z = zeros(size(x));
+    % variable to hold the vel
+    v = V0;
+    
+    % to hold yaw
+    theta = THETA0;
+    
+    x(:,1) = x0;
+    z(:,1) = h(x0) + nu *randn(2,1);
+    %% true traj 
+    % iteration!
+    for ind = 2:1:length(t)
+        % get acc profile
+        a = acc(t(ind));
+        
+        % euler integration
+        v = v + a* dt;
+        
+        % convert it to angular velocity,
+        % to get a perfect circle trajectory
+        omega = v / R0;
+        
+        theta = theta - omega * dt;
+        
+        % and update the pos
+        x(:,ind) = Origin + R0 * [cos(theta);sin(theta)];
+        % measurement
+        z(:,ind) = h(x(:,ind)) + nu*randn(2,1);
+        % if returned
+        if (ind>64*60)&& (theta<-2*pi)
+            break
+        end
+    end
+    % crop
+    t = t(1:ind);
+    x = x(:,1:ind);
+    z = z(:,1:ind);
+end
---
+++ problem3ye/problem3_a.m
@@ -0,0 +1,105 @@
+clc;
+clear;
+close all;
+addpath('functions_p3');
+%%  System modeling
+
+
+invM=diag([0^2,0^2]);
+% measuremebt noise
+V=diag([1,1]*DEG2RAD).^2;
+x_est_before_NR=[15;23];
+invM_before_NR=diag([0^2,0^2]);
+
+F = [0 0; 0 0];         % Continuous System Matrix
+Ts = 1;                 % Sampling Time
+Phi = eye(2) + F*Ts;      % Discretized System Matrix
+Gd = [1;1]*Ts;              
+W = 10^2;  % km/s/sqrt(hz)
+Wd=W/Ts;
+
+%%
+[x_true,t,z] = truetraj();
+N=length(x_true);
+
+
+figure(1);
+f1=plot(x_true(2,1:N),x_true(1,1:N),'r','LineWidth',2);
+axis equal
+xlabel('y');ylabel('x');
+figure(2);
+subplot(2,1,1);
+plot(z(1,:));
+subplot(2,1,2);
+plot(z(2,:));
+
+
+
+%% x_est0 ±¸ÇÏ±â
+
+[x_est_after_NR,M_after_NR]=NewtonRhapson(x_est_before_NR,invM_before_NR,z(:,1),V)
+
+
+
+%% Kalman filter
+
+
+x_est=zeros(2,N-1);
+x_error=zeros(2,N-1);
+cov_list=zeros(2,N-1);
+
+
+x_est_temp=x_est_after_NR;
+P=M_after_NR;
+
+waypoint=x_est_temp;
+for i=1:N-1
+    
+    %prediction
+    
+    x_est_temp=expm(F*Ts)*x_est_temp;
+    M=Phi*P*Phi'+Gd*Wd*Gd';
+    
+    
+    
+    %measurement update
+    H=Jacob_h(x_est_temp);
+    P=inv(inv(M)+H'*inv(V)*H);
+    K=P*H'*inv(V);
+    x_est_temp=x_est_temp+K*(z(:,i+1)-h(x_est_temp));    
+    
+    x_est(:,i)=x_est_temp;
+    x_error(:,i)=x_est(:,i)-x_true(:,i);
+    cov_list(:,i)=diag(P);
+    waypoint=[waypoint,x_est_temp];
+    
+    [evector,eval]=eig(P);
+    eli=elipse(evector,3.*sqrt(eval),x_est_temp);
+    
+    
+    figure(1); hold on;
+%     f1=scatter(x_est_temp(2),x_est_temp(1),'b');hold on;
+%     f2=plot(x_true(2,1),x_true(1,1),'r*');
+%     f3=plot(eli(2,:),eli(1,:),'b');
+    
+end
+
+f4=plot(waypoint(2,1),waypoint(1,1),'k^');
+f5=plot(waypoint(2,:),waypoint(1,:),'k');
+legend([f1,f4,f5],'true','start','est','Location','Best');xlabel('y(km)');ylabel('x(km)');
+title('3(a) Kalman Filter');
+axis equal
+
+
+
+figure(3);
+subplot(2,1,1);
+plot(abs(x_error(1,:)));hold on;
+plot(3*sqrt(cov_list(1,:)));
+xlabel('sec');ylabel('km');
+legend('error','3\sigma');
+subplot(2,1,2);
+plot(abs(x_error(2,:)));hold on;
+plot(3*sqrt(cov_list(2,:)));
+xlabel('sec');ylabel('km');
+sgtitle('3(a) Kalman Filter');
