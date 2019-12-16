16bca1a80324163e3f99b133145bec6f62b32427
yejun1204 <yejun1204@gmail.com>
2019-12-16 19:52:03 +0900

    problem 1(a) Newton Rhapson function

--- problem1/problem1_a.m
+++ problem1/problem1_a.m
@@ -4,53 +4,35 @@ close all;
 addpath('functions_p1');
 
 N=3;
-% x0 ~ N(x_mean,M)
-x_mean=[20;30]; %km
-invM=diag([0^2,0^2]);
+
 % measuremebt noise
 V=diag([1,1]*DEG2RAD).^2;
-x_est_temp0=[15;23];
-
-
+% x0 ~ (x_est0,inf);
+x_est0=[15;23];
+invM=diag([0^2,0^2]);
 %% simulation
-x_true=repmat(x_mean,1,N);
+x_true=repmat([20;30],1,N);
 Zmea=zeros(2,N);
 for i=1:N
     Zmea(:,i)=h(x_true(:,i))+sqrt(V)*randn(2,1);
 end
-%% random
+%% Newton-Rhapson
 
 x_est=zeros(2,N);
 for i=1:N
-    x_est_temp=x_est_temp0;
-%     x_est_temp=estXfromZ(Zmea(:,i));
-    k=0;
-    waypoint=x_est_temp;
-    while(true)
-        
-        Z_est=h(x_est_temp);
-%         J(k)=1/2*(x_est_temp-x_mean)'*invM*(x_est_temp-x_mean)+1/2*(Zmea(:,i)-Z_est)'*inv(V)*(Zmea(:,i)-Z_est);
-        k=k+1;
-        H_est=Jacob_h(x_est_temp);
-        P=inv(invM+H_est'*inv(V)*H_est);
-        GRD=invM*(x_est_temp-x_mean)-H_est'*inv(V)*(Zmea(:,i)-Z_est);
-        norm(GRD)
-        if(norm(GRD)<0.0000000001)
-            k
-            break;
-        end
-        x_est_temp=x_est_temp-P*GRD;
-        waypoint=[waypoint,x_est_temp]; 
-    end
-
-    x_est(:,i)=x_est_temp;
-    [evector,eval]=eig(P);
-
     
-     eli=elipse(evector,3.*eval,x_est(:,i));
+    [x_est,P,waypoint]=NewtonRhapson(x_est0,invM,Zmea(:,i),V)
+    
+    
+    
+    [evector,eval]=eig(P);
+    eli=elipse(evector,3.*eval,x_est);
+    
+    
+    
     figure(1); hold on;
-    scatter(x_est(2,i),x_est(1,i),'b');hold on;
-    plot(x_true(2,:),x_true(1,:),'r*');
+    scatter(x_est(2),x_est(1),'b');hold on;
+    plot(x_true(2,1),x_true(1,1),'r*');
     legend('est','true');
     plot(eli(2,:),eli(1,:),'b');
     plot(waypoint(2,:),waypoint(1,:),'k');
@@ -58,4 +40,6 @@ for i=1:N
     legend('est','true','3\sigma','Newton-Rhapson','Location','Best');
     xlabel('y(km)');ylabel('x(km)');
 end
+
+
 
