0944976b46753e5f658e7d37255879fbcc44b165
hyunwoong kang <hwoongkang@snu.ac.kr>
2019-12-16 14:38:42 +0900

    acc profile

---
+++ scripts/prob4.m
@@ -0,0 +1,23 @@
+%% prob 4.
+
+%% discretization
+% continuous state matrix
+A = [0,1,0,0;
+    0,0,0,0;
+    0,0,0,1;
+    0,0,0,0];
+
+% continuous Gamma Matrix (with Q_c = W)
+gamma = [0;1;0;1];
+
+% Gamma * W * Gamma^T
+Q = gamma * gamma.';
+
+% Van Loan's Discretization
+C = expm([-A,Q;zeros(4), A.']);
+
+Ad = C(5:8,5:8).';
+Qd = Ad * C(1:4,5:8);
+
+
+%% part (a)
